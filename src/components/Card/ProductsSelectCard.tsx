e398b0bda409d104b615f9b70e28fed37ab5cc57
qiuliw <1807191473@qq.com>
2024-06-05 10:07:48 +0800

    t

--- src/components/Card/ProductsSelectCard.tsx
+++ src/components/Card/ProductsSelectCard.tsx
@@ -100,7 +100,14 @@ export default function ProductsSelectCard() {
                                 style={{ width: 100 }}
                                 listHeight={230}
                                 options={[
-
+                                    { value: '全部', label: '全部' },
+                                    { value: '商品名称', label: '商品名称' },
+                                    { value: '商品SPU', label: '商品SPU' },
+                                    { value: '商品SKU', label: '商品SKU' },
+                                    { value: '商品厂商', label: '商品厂商' },
+                                    { value: '商品条码', label: '商品条码' },
+                                    { value: '规格名称', label: '规格名称' },
+                                    { value: '商品描述', label: '商品描述' },
                                 ]}
                             />
                             <Search
